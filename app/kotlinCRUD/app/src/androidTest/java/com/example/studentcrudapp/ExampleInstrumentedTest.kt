a26dfa0f69458dd0e8732a636e6edc45d5107f63
Mackrina10 <heavenmhally@gmail.com>
2026-01-30 09:11:36 +0300

    Add data files

---
+++ app/kotlinCRUD/app/src/androidTest/java/com/example/studentcrudapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <title>REVISION</title>
+        <h1>Hi</h1>
+        <h2>Don't Give Up</h2>
+        <h3>Pray God And Do More And More</h3>
+    </body>
+</html>
