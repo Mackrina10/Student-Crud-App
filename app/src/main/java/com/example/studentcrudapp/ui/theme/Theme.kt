0bce890a9f87d2acf4505bf3aa7a9e0d5ead2270
Mackrina10 <heavenmhally@gmail.com>
2026-01-31 12:14:59 +0300

    Add studentDao.kt

--- app/src/main/java/com/example/studentcrudapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/studentcrudapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.studentcrudappnow.ui.theme
+package com.example.studentcrudapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/example/studentcrudapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/studentcrudapp/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.studentcrudappnow.ui.theme
+package com.example.studentcrudapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
